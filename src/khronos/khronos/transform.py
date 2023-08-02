f7f55d7ad0767e435caa4ee35e29121bfcfa1913
Stef Ruinard <stefruinard@hotmail.com>
2023-08-02 12:48:50 +0200

    Add basic preprocessing.

---
+++ src/khronos/khronos/transform.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+def transform_dataset_to_tick_per_row(df: pd.DataFrame):
+    # df is a multi-colum dataframe with the following structure:
+    #   - index: datetime
+    #   - columns: multi-index with levels:
+    #       - level 0: ticker   (e.g. 'AAPL', 'MSFT')
+    #       - level 1: feature  (e.g. 'Open', 'High', 'Low', 'Close', 'Volume')
+
+    # We want to transform this dataframe into a single-column dataframe with the following structure:
+    # - index: multi-index with levels:
+    #       - level 0: ticker   (e.g. 'AAPL', 'MSFT')
+    #       - level 1: datetime
+
+    # - columns:
+    #       - 'Open'
+    #       - 'High'
+    #       - 'Low'
+    #       - 'Close'
+    #       - 'Volume'
+
+    # We can achieve this by:
+    ticker_on_rows_df = df.stack(level=0)
+    # set timestamp to datetime
+    ticker_on_rows_df.index = ticker_on_rows_df.index.set_levels(pd.to_datetime(ticker_on_rows_df.index.levels[0]), level=0)
+    # swap levels
+    ticker_at_level_0_df = ticker_on_rows_df.swaplevel().sort_index()
+    return ticker_at_level_0_df
+
+def flatten_dataset(df: pd.DataFrame):
+    date_and_ticker_index_df = df.stack(level=0)
+    date_and_ticker_index_df['ticker'] = date_and_ticker_index_df.index.get_level_values(1)
+    # drop the ticker index
+    date_and_ticker_index_df.index = date_and_ticker_index_df.index.droplevel(1)
+    return date_and_ticker_index_df
+
+
+
+def to_multi_index_df(df: pd.DataFrame):
+    # add ticker as index level 0 and datetime as index level 1
+    date_and_ticker_index_df = df.set_index(['ticker'], append=True)
+    date_and_ticker_index_df.index = date_and_ticker_index_df.index.swaplevel()
+    date_and_ticker_index_df.sort_index(inplace=True)
+    return date_and_ticker_index_df
+
+def add_timestamp_in_seconds_as_raw_input(df: pd.DataFrame):
+    # get index values
+    datetime_values = df.index.values
+    # convert to seconds
+    timestamp_in_seconds = datetime_values.astype(np.int64) // 10**9
+    # add as input call 'timestamp_in_seconds'
+    df['timestamp_in_seconds'] = timestamp_in_seconds
+    return df
+
---
+++ src/khronos/tests/test_transform.py
@@ -0,0 +1,52 @@
+import pytest
+import pandas as pd
+from khronos import transform
+
+@pytest.fixture
+def raw_sp500_aapl_and_msft_2018_dataframe():
+    dataset = pd.read_csv('./artifacts/test/raw.csv', index_col=0, header=[0, 1]).sort_index(axis=1)
+    return dataset
+
+@pytest.fixture
+def flat_df():
+    data = {'ticker': ['AAPL', 'AAPL', 'AAPL', 'MSFT', 'MSFT', 'MSFT'],
+            'datetime': ['2018-01-02 09:30:00', '2018-01-02 09:30:00', '2018-01-02 09:30:00',
+                            '2018-01-02 09:30:00', '2018-01-02 09:30:00', '2018-01-02 09:30:00'],
+            'Open': [170.16, 170.16, 170.16, 86.13, 86.13, 86.13],
+            'High': [170.16, 170.16, 170.16, 86.13, 86.13, 86.13],
+            'Low': [170.16, 170.16, 170.16, 86.13, 86.13, 86.13],
+            'Close': [170.16, 170.16, 170.16, 86.13, 86.13, 86.13],
+            'Volume': [0, 0, 0, 0, 0, 0]}
+    df = pd.DataFrame(data)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df.set_index(['datetime'], inplace=True)
+
+    return df
+
+def test_flatten_dataframe_to_tick_per_row(raw_sp500_aapl_and_msft_2018_dataframe):
+    flat_df = transform.flatten_dataset(raw_sp500_aapl_and_msft_2018_dataframe)
+    assert 'ticker' in flat_df.columns
+    assert len(flat_df.index.names) == 1
+
+def test_transform_dataframe_to_tick_per_row(raw_sp500_aapl_and_msft_2018_dataframe):
+    flat_df = transform.flatten_dataset(raw_sp500_aapl_and_msft_2018_dataframe)
+    tick_per_row = transform.to_multi_index_df(flat_df)
+    # tick_per_row = transform.transform_dataset_to_tick_per_row(raw_sp500_aapl_and_msft_2018_dataframe)
+    
+    # select AAPL and datetime is between 2018-01-02 09:30:00 and 2018-01-02 16:00:00
+    assert tick_per_row.loc['AAPL', '2018-01-02 09:30:00':'2018-01-02 09:32:00', :].shape == (3, 5)
+
+    # assert msft and aapl are in the index
+    assert 'AAPL' in tick_per_row.index
+    assert 'MSFT' in tick_per_row.index
+    
+def test_add_timestamp_in_seconds_as_raw_input(flat_df):
+    df = transform.add_timestamp_in_seconds_as_raw_input(flat_df)
+
+    # convert seconds to datetime and check if it is equal to the datetime index
+    seconds = df['timestamp_in_seconds'].apply(lambda x: pd.to_datetime(x, unit='s'))
+    # assert all true
+    assert (seconds == df.index).all()
+
+
+
